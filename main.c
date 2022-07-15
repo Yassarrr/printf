a567434e99ec2c4fda1d9393afb76baefb864aa1
Abdulyassa Murtala <yassar307@yahoo.com>
2022-07-15 15:53:54 -0700

    testrun

--- main.c
+++ main.c
@@ -1,16 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "main.h"
 
 /**
  * main - Entry point
  *
- * Return: Always 0
+ * Return: 0 on success, error code otherwise
  */
-
 int main(void)
-
 {
-	_printf("%S\n", "Best\nSchool");
+	int len, len2;
 
+	len = _printf("Let's print a simple sentence.\n");
+	len2 = printf("Let's print a simple sentence.\n");
+	fflush(stdout);
+	if (len != len2)
+	{
+		printf("Lengths differ.\n");
+		fflush(stdout);
+		return (1);
+	}
 	return (0);
-
 }
